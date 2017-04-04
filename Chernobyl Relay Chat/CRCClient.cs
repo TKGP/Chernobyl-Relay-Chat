93850a8a105abc9b49b44bb7e05584f9242e6a65
SanCampos <thedrunkengungan@gmail.com>
2017-04-04 15:51:52 +0800

    Update CRCClient.cs

--- Chernobyl Relay Chat/CRCClient.cs
+++ Chernobyl Relay Chat/CRCClient.cs
@@ -116,9 +116,9 @@ namespace Chernobyl_Relay_Chat
 
         public static void Send(string message)
         {
-                client.SendMessage(SendType.Message, CRCOptions.ChannelProxy(), message);
-                CRCDisplay.OnOwnChannelMessage(CRCOptions.Name, message);
-                CRCGame.OnChannelMessage(CRCOptions.Name, CRCOptions.GetFaction(), message);
+            client.SendMessage(SendType.Message, CRCOptions.ChannelProxy(), message);
+            CRCDisplay.OnOwnChannelMessage(CRCOptions.Name, message);
+            CRCGame.OnChannelMessage(CRCOptions.Name, CRCOptions.GetFaction(), message);
         }
 
         public static void SendDeath(string message)
